0342b418700b15ec2479399e17f2abc2f3776f35
Jeanette Yue <yue.jeanette@gmail.com>
2020-04-15 22:50:31 -0700

    Add id key to nested substitutes for each category to keep track of favorites

--- src/data.js
+++ src/data.js
@@ -10,22 +10,26 @@ const BAKING_POWDER = {
   yield: [1, "tsp"],
   substitutes: [
     {
+      id: 1,
       ingredient1: ["baking soda", 0.25, "tsp"],
       ingredient2: ["cream of tartar", 0.25, "tsp"],
       ingredient3: ["cornstarch", 0.25, "tsp"],
       vegan: true,
     },
     {
+      id: 2,
       ingredient1: ["buttermilk", 0.5, "cup"],
       ingredient2: ["baking soda", 0.25, "tsp"],
       vegan: false,
     },
     {
+      id: 3,
       ingredient1: ["molasses", 0.25, "cup"],
       ingredient2: ["baking soda", 0.25, "tsp"],
       vegan: true,
     },
     {
+      id: 4,
       ingredient1: ["sour milk", 0.5, "cup"],
       ingredient2: ["baking soda", 0.25, "tsp"],
       vegan: false,
@@ -38,11 +42,9 @@ const BAKING_SODA = {
   purpose: "leavening",
   yield: [1, "tsp"],
   substitutes: [
+    { id: 1, ingredient1: ["baking powder", 4, "tsp"], vegan: true },
     {
-      ingredient1: ["baking powder", 4, "tsp"],
-      vegan: true,
-    },
-    {
+      id: 2,
       ingredient1: ["potassium bicarbonate", 1, "tsp"],
       ingredient2: ["salt", 0.333, "tsp"],
       vegan: true,
@@ -55,20 +57,15 @@ const EGG = {
   purpose: "leavening agent, binder, texture",
   yield: [1, "egg"],
   substitutes: [
+    { id: 1, ingredient1: ["applesauce", 0.25, "cup"], vegan: true },
     {
-      ingredient1: ["applesauce", 0.25, "cup"],
-      vegan: true,
-    },
-    {
+      id: 2,
       ingredient1: ["ground flaxseed", 1, "tbs"],
       ingredient2: ["water", 3, "tbs"],
       vegan: true,
     },
-    {
-      ingredient1: ["pureed silken tofu", 0.25, "cup"],
-      vegan: true,
-    },
-    { ingredient1: ["mayo", 3, "tbs"], vegan: true },
+    { id: 3, ingredient1: ["pureed silken tofu", 0.25, "cup"], vegan: true },
+    { id: 4, ingredient1: ["mayo", 3, "tbs"], vegan: true },
   ],
 };
 
@@ -77,22 +74,22 @@ const BUTTERMILK = {
   purpose: "leavening, tenderness, flavor, texture",
   yield: [1, "cup"],
   substitutes: [
+    { id: 1, ingredient1: ["plain yogurt", 1, "cup"], vegan: false },
     {
-      ingredient1: ["plain yogurt", 1, "cup"],
-      vegan: false,
-    },
-    {
+      id: 2,
       ingredient1: ["milk", 1, "cup"],
       ingredient2: ["vinegar (white or cider)", 1, "tbs"],
       vegan: false,
     },
     {
+      id: 3,
       ingredient1: ["milk", 1, "cup"],
       ingredient2: ["lemon juice", 1, "tbs"],
       vegan: false,
     },
-    { ingredient1: ["sour cream", 1, "cup"], vegan: false },
+    { id: 4, ingredient1: ["sour cream", 1, "cup"], vegan: false },
     {
+      id: 5,
       ingredient1: ["milk", 1, "cup"],
       ingredient2: ["cream of tartar (potassium bitartrate", 1.5, "tsp"],
       vegan: false,
@@ -105,21 +102,21 @@ const WHOLE_MILK = {
   purpose: "leavening, tenderness, flavor, texture",
   yield: [1, "cup"],
   substitutes: [
+    { id: 1, ingredient1: ["evaporated whole milk", 0.5, "cup"], vegan: false },
     {
-      ingredient1: ["evaporated whole milk", 0.5, "cup"],
-      vegan: false,
-    },
-    {
+      id: 2,
       ingredient1: ["condensed milk", 0.5, "cup"],
       ingredient2: ["water", 0.5, "cup"],
       vegan: false,
     },
     {
+      id: 3,
       ingredient1: ["skim milk", 1, "cup"],
       ingredient2: ["melted butter", 2, "tbs"],
       vegan: false,
     },
     {
+      id: 4,
       ingredient1: ["skim milk", 1, "cup"],
       ingredient2: ["melted margarine", 2, "tbs"],
       vegan: false,
@@ -132,23 +129,11 @@ const MILK = {
   purpose: "leavening, tenderness, flavor, texture",
   yield: [1, "cup"],
   substitutes: [
-    {
-      ingredient1: ["unsweetened soymilk", 1, "cup"],
-      vegan: true,
-    },
-    {
-      ingredient1: ["unsweeteed oat milk", 1, "cup"],
-      vegan: true,
-    },
-    {
-      ingredient1: ["unsweetened rice milk", 1, "cup"],
-      vegan: true,
-    },
-    { ingredient1: ["coconut milk", 1, "cup"], vegan: true },
-    {
-      ingredient1: ["plain yogurt", 1, "cup"],
-      vegan: false,
-    },
+    { id: 1, ingredient1: ["unsweetened soymilk", 1, "cup"], vegan: true },
+    { id: 2, ingredient1: ["unsweeteed oat milk", 1, "cup"], vegan: true },
+    { id: 3, ingredient1: ["unsweetened rice milk", 1, "cup"], vegan: true },
+    { id: 4, ingredient1: ["coconut milk", 1, "cup"], vegan: true },
+    { id: 5, ingredient1: ["plain yogurt", 1, "cup"], vegan: false },
   ],
 };
 
@@ -157,26 +142,11 @@ const BUTTER = {
   purpose: "fat, flavor, moisture retention",
   yield: [1, "cup"],
   substitutes: [
-    {
-      ingredient1: ["vegetable oil", 1, "cup"],
-      vegan: true,
-    },
-    {
-      ingredient1: ["coconut oil", 1, "cup"],
-      vegan: true,
-    },
-    {
-      ingredient1: ["margarine", 1, "cup"],
-      vegan: true,
-    },
-    {
-      ingredient1: ["lard", 1, "cup"],
-      vegan: false,
-    },
-    {
-      ingredient1: ["avocado", 1, "cup"],
-      vegan: true,
-    },
+    { id: 1, ingredient1: ["vegetable oil", 1, "cup"], vegan: true },
+    { id: 2, ingredient1: ["coconut oil", 1, "cup"], vegan: true },
+    { id: 3, ingredient1: ["margarine", 1, "cup"], vegan: true },
+    { id: 4, ingredient1: ["lard", 1, "cup"], vegan: false },
+    { id: 5, ingredient1: ["avocado", 1, "cup"], vegan: true },
   ],
 };
 
@@ -186,34 +156,32 @@ const YEAST = {
   yield: [1, "tsp"],
   substitutes: [
     {
+      id: 1,
       ingredient1: ["baking soda", 0.5, "tsp"],
       ingredient2: ["lemon juice", 0.5, "tsp"],
       vegan: true,
     },
     {
+      id: 2,
       ingredient1: ["baking soda", 0.5, "tsp"],
       ingredient2: ["buttermilk", 0.5, "tsp"],
       vegan: false,
     },
     {
+      id: 3,
       ingredient1: ["baking soda", 0.5, "tsp"],
       ingredient2: ["cream of tartar", 0.5, "tsp"],
       vegan: true,
     },
     {
+      id: 4,
       ingredient1: ["baking soda", 0.5, "tsp"],
       ingredient2: ["milk", 0.25, "tsp"],
       ingredient3: ["vinegar", 0.25, "tsp"],
       vegan: false,
     },
-    {
-      ingredient1: ["baking powder", 1, "tsp"],
-      vegan: true,
-    },
-    {
-      ingredient1: ["sourdough starter", 0.5, "cup"],
-      vegan: true,
-    },
+    { id: 5, ingredient1: ["baking powder", 1, "tsp"], vegan: true },
+    { id: 6, ingredient1: ["sourdough starter", 0.5, "cup"], vegan: true },
   ],
 };
 
@@ -223,33 +191,31 @@ const HEAVY_CREAM = {
   yield: [1, "cup"],
   substitutes: [
     {
+      id: 1,
       ingredient1: ["melted butter", 0.25, "cup"],
       ingredient2: ["milk", 0.75, "cup"],
       vegan: false,
     },
     {
+      id: 2,
       ingredient1: ["olive oil", 0.33, "cup"],
       ingredient2: ["soymilk", 0.67, "cup"],
       vegan: true,
     },
     {
+      id: 3,
       ingredient1: ["milk", 1, "cup"],
       ingredient2: ["cornstarch", 2, "tbs"],
       vegan: false,
     },
     {
+      id: 4,
       ingredient1: ["blended silk tofu", 0.5, "cup"],
       ingredient2: ["milk", 0.5, "cup"],
       vegan: true,
     },
-    {
-      ingredient1: ["evaporated milk", 1, "cup"],
-      vegan: false,
-    },
-    {
-      ingredient1: ["coconut cream", 1, "cup"],
-      vegan: true,
-    },
+    { id: 5, ingredient1: ["evaporated milk", 1, "cup"], vegan: false },
+    { id: 6, ingredient1: ["coconut cream", 1, "cup"], vegan: true },
   ],
 };
 
@@ -259,6 +225,7 @@ const SELF_RISING_FLOUR = {
   yield: [1, "cup"],
   substitutes: [
     {
+      id: 1,
       ingredient1: ["all-purpose flour", 1, "cup"],
       ingredient2: ["baking powder", 1.5, "tsp"],
       ingredient3: ["salt", 0.25, "tsp"],
@@ -273,6 +240,7 @@ const CAKE_FLOUR = {
   yield: [1, "cup"],
   substitutes: [
     {
+      id: 1,
       ingredient1: ["sifted all-purpose flour", 0.75, "cup"],
       ingredient2: ["cornstarch", 2, "tbs"],
       vegan: true,
