00632f5a3272d5e01de4d2d7360912ece58c5bb2
Jeanette Yue <yue.jeanette@gmail.com>
2020-04-17 21:57:36 -0700

    Add settings component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import Radio from "@material-ui/core/Radio";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const [vegan, setVegan] = useState(false);
+  const [theme, setTheme] = useState("themeA");
+
+  return (
+    <React.Fragment>
+      <h1 style={{ margin: "auto" }}>Settings</h1>
+      <Grid container direction="column">
+        <Grid item>
+          <h2>Vegan</h2>
+          <Switch checked={vegan} onChange={() => setVegan(!vegan)} />
+          {vegan && <p>hide nonvegan substitutes</p>}
+          {!vegan && <p>show nonvegan substitutes</p>}
+        </Grid>
+        <Grid item>
+          <h2>Themes</h2>
+          <FormControl component="fieldset">
+            {/* <FormLabel component="legend">Gender</FormLabel> */}
+            <RadioGroup
+              aria-label="themes"
+              // name="gender1"
+              value={theme}
+              onChange={(e) => setTheme(e.target.value)}
+              style={{
+                width: "100%",
+                display: "flex",
+                flexFlow: "row wrap",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <FormControlLabel
+                value="themeA"
+                control={<Radio color="primary" />}
+                style={{
+                  width: "fit-content",
+                  margin: 0,
+                }}
+              />
+              <FormControlLabel
+                value="themeB"
+                control={<Radio color="secondary" />}
+                style={{ width: "fit-content", margin: 0 }}
+              />
+              <FormControlLabel
+                value="themeC"
+                control={<Radio color="secondary" />}
+                style={{ width: "fit-content", margin: 0 }}
+              />
+              <FormControlLabel
+                value="themeD"
+                control={<Radio color="secondary" />}
+                style={{
+                  width: "fit-content",
+                  margin: 0,
+                }}
+              />
+            </RadioGroup>
+          </FormControl>
+          <h3>selected theme: {theme}</h3>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 };
 
 export default Settings;
