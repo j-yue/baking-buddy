587d50d08d3f0be0c704de24ce5cb9f4dfe16df5
Jeanette Yue <yue.jeanette@gmail.com>
2020-04-20 12:24:34 -0700

    Remove nested ThemeProviders
    - Rename theme.js to palettes.js, then give the exported palettes
    the alias PALETTES
    
    - Create new theme.js file with the function createTheme, a wrapper for
    createMuiTheme to create themes with fixed typography rules but
    different palettes
    
    - Add useEffect hook to update the theme passed to ThemeProvider
    whenever the selected palette has changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route, useHistory, useLocation } from "react-router-dom";
 
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import "./App.css";
@@ -15,44 +15,8 @@ import Settings from "./components/Settings";
 import Substitute from "./components/Substitute";
 import SUBSTITUTES from "./data";
 
-import * as CAKES from "./themes.js";
-
-const THEME = createMuiTheme({
-  typography: {
-    fontFamily: [
-      "Ubuntu",
-      "Noto Sans",
-      "sans-serif",
-      "Raleway",
-      "Oxygen",
-      "Didact Gothic",
-    ].join(","),
-    fontSize: 10,
-
-    h1: {
-      fontSize: "2rem",
-    },
-    body1: {
-      fontFamily: [
-        "Didact Gothic",
-        "Raleway",
-        "Oxygen",
-        "Didact Gothic",
-        "sans-serif",
-      ].join(","),
-      fontSize: "1.25rem",
-    },
-    body2: {
-      fontSize: "1rem",
-    },
-    subtitle1: {
-      fontWeight: "bold",
-      color: "#525A33",
-      fontSize: "1.5rem",
-      textTransform: "uppercase",
-    },
-  },
-});
+import * as PALETTES from "./palettes.js";
+import { createTheme } from "./theme.js";
 
 const headerFromPath = (path) => {
   const pathName = path.pathname;
@@ -73,86 +37,86 @@ const App = () => {
   const history = useHistory();
   const location = useLocation();
   const [vegan, setVegan] = useState(false);
-  // const [theme, setTheme] = useState("lemonMeringue");
   const [palette, setPalette] = useState("lemonMeringue");
+  const [theme, setTheme] = useState(createTheme(palette));
   const [screen, setScreen] = useState(headerFromPath(location));
 
-  //might need to add useeffect hook
-  let muiPalette = CAKES[palette];
+  //whenever palette changes update the theme
+  useEffect(() => {
+    setTheme(createTheme(palette));
+  }, [palette]);
 
   useEffect(() => {
     setScreen(headerFromPath(location));
   }, [location]);
 
   return (
-    <ThemeProvider theme={createMuiTheme({ palette: muiPalette.palette })}>
-      <ThemeProvider theme={THEME}>
-        <CssBaseline />
-        <Container>
-          <Grid
-            container
-            className="App"
-            style={{ height: "100vh" }}
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item style={{ height: "20%", width: "100%" }}>
-              <Typography variant="h1" component="h1">
-                {screen}
-              </Typography>
-            </Grid>
-            <Grid item style={{ height: "80%", width: "100%" }}>
-              <Switch>
-                <Route path="/" component={Home} exact />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Grid
+          container
+          className="App"
+          style={{ height: "100vh" }}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item style={{ height: "20%", width: "100%" }}>
+            <Typography variant="h1" component="h1">
+              {screen}
+            </Typography>
+          </Grid>
+          <Grid item style={{ height: "80%", width: "100%" }}>
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route
+                path="/faves"
+                component={() => (
+                  <Faves
+                    favorites={favorites}
+                    setFavorites={setFavorites}
+                    data={SUBSTITUTES}
+                  />
+                )}
+                exact
+              />
+              <Route
+                path="/settings"
+                component={() => (
+                  <Settings
+                    palette={palette}
+                    setPalette={setPalette}
+                    allPalettes={PALETTES}
+                    // cake={cake}
+                    // setCake={setCake}
+                    vegan={vegan}
+                    setVegan={setVegan}
+                  />
+                )}
+                exact
+              />
+              <Route path="/search" component={Search} exact />
+
+              {Object.keys(SUBSTITUTES).map((key) => (
                 <Route
-                  path="/faves"
+                  path={`/substitute/${key}`}
                   component={() => (
-                    <Faves
+                    <Substitute
+                      ingredient={key}
+                      data={SUBSTITUTES[key]}
                       favorites={favorites}
                       setFavorites={setFavorites}
-                      data={SUBSTITUTES}
                     />
                   )}
                   exact
+                  key={key}
                 />
-                <Route
-                  path="/settings"
-                  component={() => (
-                    <Settings
-                      palette={palette}
-                      setPalette={setPalette}
-                      allPalettes={CAKES}
-                      // cake={cake}
-                      // setCake={setCake}
-                      vegan={vegan}
-                      setVegan={setVegan}
-                    />
-                  )}
-                  exact
-                />
-                <Route path="/search" component={Search} exact />
-
-                {Object.keys(SUBSTITUTES).map((key) => (
-                  <Route
-                    path={`/substitute/${key}`}
-                    component={() => (
-                      <Substitute
-                        ingredient={key}
-                        data={SUBSTITUTES[key]}
-                        favorites={favorites}
-                        setFavorites={setFavorites}
-                      />
-                    )}
-                    exact
-                    key={key}
-                  />
-                ))}
-              </Switch>
-            </Grid>
+              ))}
+            </Switch>
           </Grid>
-        </Container>
-      </ThemeProvider>
+        </Grid>
+      </Container>
     </ThemeProvider>
   );
 };
---
+++ src/theme.js
@@ -0,0 +1,46 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import * as PALETTES from "./palettes.js";
+
+//create theme with dynamic palette
+//colors is a palette object created by createPalette in palettes.js
+const createTheme = (colors) => {
+  const { palette } = PALETTES[colors];
+  return createMuiTheme({
+    palette,
+    typography: {
+      fontFamily: [
+        "Ubuntu",
+        "Noto Sans",
+        "sans-serif",
+        "Raleway",
+        "Oxygen",
+        "Didact Gothic",
+      ].join(","),
+      fontSize: 10,
+    },
+    h1: {
+      fontSize: "2rem",
+    },
+    body1: {
+      fontFamily: [
+        "Didact Gothic",
+        "Raleway",
+        "Oxygen",
+        "Didact Gothic",
+        "sans-serif",
+      ].join(","),
+      fontSize: "1.25rem",
+    },
+    body2: {
+      fontSize: "1rem",
+    },
+    subtitle1: {
+      fontWeight: "bold",
+      color: "#525A33",
+      fontSize: "1.5rem",
+      textTransform: "uppercase",
+    },
+  });
+};
+
+export { createTheme };
